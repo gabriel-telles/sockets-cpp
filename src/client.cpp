c205ed831fc2002ef0c5d78ba36e23740ba588f5
telles <gabrieltellesm@gmail.com>
2024-09-11 22:19:02 -0300

    Refactor client code to improve readability

--- src/client.cpp
+++ src/client.cpp
@@ -1,4 +1,5 @@
 #include "client.h"
+
 #include <cstring>
 #include <iostream>
 #include <unistd.h>
@@ -10,26 +11,66 @@
 
 using namespace std;
 
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        displayErrorAndExit("Usage: client <server-name> <command> [file-name]");
+    }
+
+    const char* server_name = argv[1];
+    const char* command = argv[2];
+    const char* file_name = (argc == 4) ? argv[3] : nullptr;
+
+    struct hostent* server_host = gethostbyname(server_name);
+    if (!server_host) {
+        displayErrorAndExit("Failed to resolve host name");
+    }
+
+    int socket_fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    handleSocketError(socket_fd);
+
+    struct sockaddr_in server_address;
+    initializeSocketAddress(server_address, server_host);
+
+    if (connect(socket_fd, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+        displayErrorAndExit("Failed to connect to server");
+    }
+
+    string request;
+    if (strcmp(command, "MyGet") == 0 && file_name) {
+        request = "MyGet " + string(file_name);
+    } else if (strcmp(command, "MyLastAccess") == 0) {
+        request = "MyLastAccess";
+    } else {
+        displayErrorAndExit("Invalid command or missing file name");
+    }
+    sendRequest(socket_fd, request);
+
+    receiveResponse(socket_fd);
+
+    close(socket_fd);
+    return 0;
+}
+
+void displayErrorAndExit(const std::string& message) {
+    perror(message.c_str());
+    exit(EXIT_FAILURE);
+}
+
 void handleSocketError(int socket_fd) {
     if (socket_fd < 0) {
         displayErrorAndExit("Socket creation failed");
     }
 }
 
-void initializeSocketAddress(struct sockaddr_in &address, struct hostent *host) {
+void initializeSocketAddress(struct sockaddr_in& address, const struct hostent* host) {
     memset(&address, 0, sizeof(address));
     address.sin_family = AF_INET;
     memcpy(&address.sin_addr.s_addr, host->h_addr, host->h_length);
     address.sin_port = htons(SERVER_PORT);
 }
 
-void displayErrorAndExit(const char *message) {
-    perror(message);
-    exit(EXIT_FAILURE);
-}
-
-void sendRequest(int socket_fd, const char *request) {
-    if (write(socket_fd, request, strlen(request)) < 0) {
+void sendRequest(int socket_fd, const std::string& request) {
+    if (write(socket_fd, request.c_str(), request.size()) < 0) {
         displayErrorAndExit("Failed to send request");
     }
 }
@@ -37,53 +78,19 @@ void sendRequest(int socket_fd, const char *request) {
 void receiveResponse(int socket_fd) {
     char buffer[BUFFER_SIZE];
     ssize_t bytes_received;
-    while ((bytes_received = read(socket_fd, buffer, BUFFER_SIZE)) > 0) {
+
+    while (true) {
+        bytes_received = read(socket_fd, buffer, sizeof(buffer) - 1);
+        if (bytes_received < 0) {
+            displayErrorAndExit("Failed to read data from socket");
+        } else if (bytes_received == 0) {
+            break; // End of stream, connection closed
+        }
+
+        buffer[bytes_received] = '\0';
         if (write(STDOUT_FILENO, buffer, bytes_received) < 0) {
             displayErrorAndExit("Failed to write data to stdout");
         }
+        fflush(stdout);
     }
-
-    if (bytes_received < 0) {
-        displayErrorAndExit("Failed to read data from socket");
-    }
-}
-
-int main(int argc, char **argv) {
-    if (argc < 3) {
-        displayErrorAndExit("Usage: client server-name command [file-name]");
-    }
-
-    int connection_fd;
-    int socket_fd;
-    struct hostent *server_host;
-    struct sockaddr_in server_address;
-
-    server_host = gethostbyname(argv[1]);
-    if (!server_host) {
-        displayErrorAndExit("Failed to resolve host name");
-    }
-
-    socket_fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    handleSocketError(socket_fd);
-
-    initializeSocketAddress(server_address, server_host);
-
-    connection_fd = connect(socket_fd, (struct sockaddr *)&server_address, sizeof(server_address));
-    if (connection_fd < 0) {
-        displayErrorAndExit("Failed to connect to server");
-    }
-
-    if (strcmp(argv[2], "MyGet") == 0 && argc == 4) {
-        char request[BUFFER_SIZE];
-        snprintf(request, sizeof(request), "MyGet %s", argv[3]);
-        sendRequest(socket_fd, request);
-    } else if (strcmp(argv[2], "MyLastAccess") == 0) {
-        sendRequest(socket_fd, "MyLastAccess");
-    } else {
-        displayErrorAndExit("Invalid command or missing file name");
-    }
-
-    receiveResponse(socket_fd);
-    close(socket_fd);
-    return 0;
 }
--- src/client.h
+++ src/client.h
@@ -6,18 +6,18 @@
 #include <netdb.h>
 #include <string>
 
-#define SERVER_PORT 8080
-#define BUFFER_SIZE 4096
+constexpr int SERVER_PORT = 8080;
+constexpr int BUFFER_SIZE = 4096;
 
-enum CommandType {
-    MYGET,
-    MYLASTACCESS
+enum class CommandType {
+    Get,
+    LastAccess
 };
 
 void handleSocketError(int socket_fd);
-void initializeSocketAddress(struct sockaddr_in &address, struct hostent *host);
-void displayErrorAndExit(const char *message);
-void sendRequest(int socket_fd, const char *request);
+void initializeSocketAddress(struct sockaddr_in& address, const struct hostent* host);
+void displayErrorAndExit(const std::string& message);
+void sendRequest(int socket_fd, const std::string& request);
 void receiveResponse(int socket_fd);
 
 #endif // CLIENT_H
